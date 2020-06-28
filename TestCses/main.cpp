7e09fa3cd94f4d4a159335a7a31a102b0e4b3704
lionel@lionel-port3 <lionel.desorme@free.fr>
2020-06-28 16:41:59 +0200

    Test file status

--- TestCses/main.cpp
+++ TestCses/main.cpp
@@ -4,8 +4,9 @@
 #include <fstream>
 
 #ifdef MY_INPUT
+#define FILE_INPUT "c:\\tmp\\input.txt"
 #define STREAM_IN my_file
-#define OPEN_IN std::fstream my_file("c:\\tmp\\input.txt")
+#define OPEN_IN std::fstream my_file(FILE_INPUT); if (!my_file.good()) { std::cout << "Error opening " << FILE_INPUT; return -1; }
 #else
 #define STREAM_IN std::cin
 #define OPEN_IN

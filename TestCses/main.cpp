b8380d0a30bdc10169b5ccff0bf964b59a0dd83a
lionel@lionel-pc2 <lionel.desorme@free.fr>
2020-06-29 13:43:56 +0200

    Test crash

--- TestCses/main.cpp
+++ TestCses/main.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <string>
 #include <fstream>
+#include <vector>
 
 #ifdef MY_INPUT
 #define FILE_INPUT "input.txt"
@@ -19,5 +20,33 @@ int main() {
     std::string s;
     STREAM_IN >> s;
    
+    std::vector<int> v(26);
+    // only zero or one letter with count odd
+    for (int i = 0; i < s.length(); i++)
+    {
+        int k = s[i] - 'A';
+        v[k]++;
+    }
+
+    std::string left;
+    std::string center;
+    std::string right;
+    int count_odd = 0;
+    for (int i = 0; i < v.size(); i++)
+    {
+        auto chunk = std::string(v[i] / 2, 'A' + i);
+        if (v[i] % 2 == 1)
+        {
+            if (center.length() > 0)
+            {
+                std::cout << "NO SOLUTION";
+                return 1;
+            }
+            center = 'A' + i;
+        }
+        left = chunk + left;
+        right = right + chunk;
+    }
+    std::cout << left << center << right;
     return 0;
 }

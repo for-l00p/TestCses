90f48682d94645518694389a9e40dcdf21b197c1
lionel@lionel-pc2 <lionel.desorme@free.fr>
2020-07-19 18:49:22 +0200

    Add pb

--- TestCses/pb_subarray_div.cpp
+++ TestCses/pb_subarray_div.cpp
@@ -18,7 +18,7 @@
 
 #ifdef MY_INPUT
 #define FILE_INPUT "input.txt"
-//#define FILE_INPUT "C:\\Users\\lione\\Downloads\\test_input (12).txt"
+#define FILE_INPUT "C:\\Users\\lione\\Downloads\\test_input (12).txt"
 //#define FILE_INPUT "C:\\Users\\lione\\Downloads\\test_input_time_limit.txt"
 //#define FILE_INPUT MY_TEST
 #define STREAM_IN my_file
@@ -66,15 +66,15 @@ static ll_type naive(const std::vector<ll_type>& sums, ll_type n)
 {
 	int count = 0;
 	for (int i = 0; i < sums.size(); i++)
-		for (int j = i+1; j < sums.size(); j++)
-	{
+		for (int j = i + 1; j < sums.size(); j++)
+		{
 			if (sums[i] == sums[j])
 				count++;
-	}
+		}
 	return count;
 }
 
-ll_type positive_remainder(ll_type v, ll_type n)
+static ll_type positive_remainder(ll_type v, ll_type n)
 {
 	auto r = v % n;
 	if (r < 0)
@@ -82,22 +82,51 @@ ll_type positive_remainder(ll_type v, ll_type n)
 	return r;
 }
 
-//https://cses.fi/problemset/task/1085
-// Array Division
-int main() {
+//https://cses.fi/problemset/task/1662
+// Subarray Divisibility
+int pb_subarray_div_main() {
 	OPEN_IN;
 
 	// n
-	ll_type n, k;
-	STREAM_IN >> n >> k;
+	ll_type n;
+	STREAM_IN >> n;
+	ll_type x = 0;
 
-	std::vector<ll_type> v(n);
+	std::map<ll_type, std::vector<int>> sums;
+	sums[0].push_back(-1);
+	ll_type current_sum = 0;
+	std::vector<ll_type> simple_sums(n + 1);
+	simple_sums[0] = 0;
 	for (int i = 0; i < n; i++)
 	{
 		ll_type k;
 		STREAM_IN >> k;
-		v[i] = k;
+		current_sum += k;
+		auto r = positive_remainder(current_sum, n);
+		sums[r].push_back(i);
+		simple_sums[i + 1] = r;
 	}
+
+	//	std::cout << naive(simple_sums, n); return 0;
+
+
+	ll_type count = 0;
+	for (auto s : sums)
+	{
+		// sj - si = x => sj = si + x et i<j !!
+		auto it = sums.find(s.first + x);
+		if (it != sums.end())
+		{
+			/*int c1 = count_couples0(s.second, it->second);
+			int c2 = count_couples(s.second, it->second);
+			if (c1 != c2)
+			{
+				int c3 = count_couples(s.second, it->second);
+			}*/
+			count += count_couples(s.second, it->second);
+		}
+	}
+	std::cout << count;
 
 #ifdef MY_INPUT
 	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
